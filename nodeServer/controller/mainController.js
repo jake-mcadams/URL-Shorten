ecb6f812bc04e3f85046ee31e8a34bed872253ee
McAdams <Jake.McAdams@alvaria.com>
2022-04-14 06:43:39 -0400

    added prependhttp logic to node, added domain check and append if none client side

--- nodeServer/controller/mainController.js
+++ nodeServer/controller/mainController.js
@@ -1,31 +1,40 @@
 // const ShortUrl = require("../model/shortUrl");
-import shortUrl from '../model/shortUrl.js';
+import ShortUrl from "../model/shortUrl.js";
 // const { nanoid } = require('nanoid');
-import { nanoid } from 'nanoid';
-import prependHttp from 'prepend-http';
+import { nanoid } from "nanoid";
+import prependHttp from "prepend-http";
 
+const getUrls = async (req, res, next) => {
+  // const shortUrls = await ShortUrl.find();
+  const urlID = nanoid(10);
+  res.json({ shorrId: urlID });
+};
 
-const getUrls = async (req, res, next)=>{
-    // const shortUrls = await ShortUrl.find();
-    const urlID = nanoid(10);
-    res.json({shorrId: urlID})
-}
-
-const getUrlsDB = async (req, res, next)=>{
-    const shortUrls = await ShortUrl.find();
-    res.json({shortUrl: shortUrls})
-}
+const getUrlsDB = async (req, res, next) => {
+  const shortUrls = await ShortUrl.find();
+  if(Object.keys(shortUrls).length === 0){
+    console.log('No urls in DB')
+  } else {
+    console.log(shortUrls)
+    console.log(typeof(shortUrls))
+    res.json({ shortUrl: shortUrls });
+  }
+};
 
 const addUrl = async (req, res, next) => {
-    if (req.body.fullUrl === null || req.body.fullUrl == "") {
-      res.sendStatus(404);
-    }
-    // await ShortUrl.create({ full: req.body.fullUrl });
-    // if(!req.body.hasProtocol){
+  if (req.body.fullUrl === null || req.body.fullUrl == "") {
+    res.sendStatus(404);
+  }
 
-    // }
-    console.log(req.body.hasProtocol)
+  console.log(req.body.fullUrl);
+  if (!req.body.hasProtocol) {
+    console.log(`Received url: ${req.body.fullUrl}`)
+    let prePendUrl = prependHttp(req.body.fullUrl);
+    await ShortUrl.create({ full: prePendUrl });
+  } else {
+    console.log(`Received url: ${req.body.fullUrl}`)
+    await ShortUrl.create({ full: req.body.fullUrl });
+  }
+};
 
-  };
-
-  export { getUrls, getUrlsDB, addUrl};
+export { getUrls, getUrlsDB, addUrl };
